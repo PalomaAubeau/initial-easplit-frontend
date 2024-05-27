b9936ee52c9032f5435023103710e2e0ee829c53
Sinda <sinda.gouret@gmail.com>
2024-05-27 11:04:21 +0200

    pull 11h04, changements Paloma

--- utils/path.js
+++ utils/path.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-//export const PATH = "http://192.168.0.12:3000";
-export const PATH = "https://easplit-backend.vercel.app";
-
-export const PATH_lastTransaction =  "https://easplit-backend.vercel.app";
-//export const PATH_lastTransaction = "http://192.168.0.12:3000"; //pourquoi ?
-
-// export const PATH = "http://192.168.42.130:3000";
-=======
 // export const PATH = "http://192.168.42.130:3000"// Béranger
 // export const PATH = "http://localhost:3000"// Paloma
 export const PATH = "https://easplit-backend.vercel.app"; // Back déployé
@@ -14,4 +5,3 @@ export const PATH = "https://easplit-backend.vercel.app"; // Back déployé
 // export const PATH_lastTransaction = "http://192.168.42.130:3000" //Béranger
 //export const PATH_lastTransaction = "http://localhost:3000" // Paloma
 export const PATH_lastTransaction = "https://easplit-backend.vercel.app"; // Back déployé
->>>>>>> e6a4e092cbd3d56ef0ca202fa180c05a42c8a1fd
